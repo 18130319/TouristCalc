8577e9332ea471b8c350d12a90fabd1638726884
18130471shakirov <18130471shakirov>
2020-05-15 05:37:56 +0500

    Шакиров Рамир добавил два класса

---
+++ touristCalc/src/main/java/org/gar/touristCalc/Initializer.java
@@ -0,0 +1,9 @@
+package org.gar.touristCalc;
+
+import org.gar.touristCalc.InterfaceAdmin;
+
+public class Initializer {
+	public static void main(String[] args) throws Exception {
+		InterfaceAdmin a = new InterfaceAdmin();
+	}
+}
