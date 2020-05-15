4705a3daec79a36bcaaa551006747f7076f07e6b
18130319 <18130319>
2020-05-15 05:50:49 +0500

    Добавление комментариев к коду

--- touristCalc/src/main/java/org/gar/touristCalc/Initializer.java
+++ touristCalc/src/main/java/org/gar/touristCalc/Initializer.java
@@ -3,7 +3,13 @@ package org.gar.touristCalc;
 import org.gar.touristCalc.InterfaceAdmin;
 
 public class Initializer {
+	/**
+	 * Запуск программы
+	 */
 	public static void main(String[] args) throws Exception {
+		/**
+		 * Создание экземпляра класса, описывающего граф. интерфейс
+		 */
 		InterfaceAdmin a = new InterfaceAdmin();
 	}
 }
