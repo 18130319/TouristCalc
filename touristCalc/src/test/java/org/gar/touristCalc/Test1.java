3e5ad4800c2f5af91812f7833142a14e0082fa6c
18130319 <18130319>
2020-05-15 06:16:43 +0500

    Гареев Данил Р. добавил два теста

---
+++ touristCalc/src/test/java/org/gar/touristCalc/Test1.java
@@ -0,0 +1,22 @@
+package org.gar.touristCalc;
+
+/**
+ * Импортирование класса, содержащего набор методов сравнения
+ */
+import static org.junit.Assert.*;
+import org.gar.touristCalc.Tour;
+public class Test1 {
+	@org.junit.Test
+	public void test1() {	
+		/**
+		 * Входными данными является метод, производящий расчета стоимости тура
+		 * и предполагаемый результат данного расчета
+		 */
+		Tour a = new Tour(2.0, 14.0, 1000, 20000, 1.34,100.0,1.78,10.0);
+		/**
+		 * Сравнение результата и ожидаемого значения
+		 */
+		assertEquals("86594.0",a.getResult());
+	}
+}
+
